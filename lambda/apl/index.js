734b29077642bf03adf6116cefcfd78b88151f80
Parker Lawrence <parker.alford@gmail.com>
2021-02-28 21:35:12 -0500

    fix compilation error

--- lambda/apl/index.js
+++ lambda/apl/index.js
@@ -11,18 +11,10 @@
  */
 
 const tokens = {
-    TITLE: 'TITLE',
-    HOME: 'HOME',
-    PATH: 'PATH',
-    PET_SHOP: 'PET_SHOP',
-    ANIMAL_TYPE_SELECTED: 'ANIMAL_TYPE_SELECTED',
-    SINGLE_ANIMAL: 'SINGLE_ANIMAL',
-    TAKE_PET_HOME: 'TAKE_PET_HOME',
     LAUNCH: 'LAUNCH',
     TRAIN: 'TRAIN',
-    ANSWER: 'ANSWER',
+    ANSWER: 'ANSWER'
 };
 
 module.exports.tokens = tokens;
-module.exports.visual = require('./visual');
 module.exports.audio = require('./audio');
